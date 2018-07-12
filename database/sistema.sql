48a76eea2c9f1ab4e1d7b5a006d77e41c11a51c6
wh0am123 <thejyjco@gmail.com>
2018-07-12 10:50:20 -0430

    Juan was here

--- database/sistema.sql
+++ database/sistema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-07-2018 a las 08:06:17
+-- Tiempo de generación: 12-07-2018 a las 17:16:54
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.2
 
@@ -37,23 +37,27 @@ CREATE TABLE `pacientes` (
   `enfermedad_actual` text NOT NULL,
   `antecedentes_familiares` text NOT NULL,
   `antecedentes_personales` text NOT NULL,
-  `inmunizaciones` date NOT NULL,
+  `inmunizaciones` text NOT NULL,
   `peso` int(100) NOT NULL,
   `talla` int(100) NOT NULL,
   `cc` int(100) NOT NULL,
   `tension_arterial` int(100) NOT NULL,
   `saturacion_oxigeno` int(100) NOT NULL,
   `tratamiento` text NOT NULL,
-  `cedula` int(100) DEFAULT NULL
+  `cedula` int(100) DEFAULT NULL,
+  `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `pacientes`
 --
 
-INSERT INTO `pacientes` (`nombres`, `apellidos`, `fecha_nacimiento`, `direccion`, `motivo_consulta`, `enfermedad_actual`, `antecedentes_familiares`, `antecedentes_personales`, `inmunizaciones`, `peso`, `talla`, `cc`, `tension_arterial`, `saturacion_oxigeno`, `tratamiento`, `cedula`) VALUES
-('Juan Pablo', 'Guzman', '0000-00-00', 'Calle Transversal Progreso', 'Rinitis', 'Rinitis', 'No', 'Enfermedad de Nacimiento', '0000-00-00', 50, 162, 50, 15, 20, 'Alprazolam', 30117262),
-('Juan', 'Guzman', '0000-00-00', 'Calle Transversal Progreso', 'Rinitis', 'Rinitis', '', 'Enfermedad de Nacimiento', '0000-00-00', 50, 162, 50, 15, 20, 'Alprazolam', 22333111);
+INSERT INTO `pacientes` (`nombres`, `apellidos`, `fecha_nacimiento`, `direccion`, `motivo_consulta`, `enfermedad_actual`, `antecedentes_familiares`, `antecedentes_personales`, `inmunizaciones`, `peso`, `talla`, `cc`, `tension_arterial`, `saturacion_oxigeno`, `tratamiento`, `cedula`, `fecha`) VALUES
+('Juan Pablo', 'Guzman', '0000-00-00', 'Calle Transversal Progreso', 'Rinitis', 'Rinitis', 'No', 'Enfermedad de Nacimiento', '0000-00-00', 50, 162, 50, 15, 20, 'Alprazolam', 30117262, '0000-00-00'),
+('Juan', 'Guzman', '0000-00-00', 'Calle Transversal Progreso', 'Rinitis', 'Rinitis', '', 'Enfermedad de Nacimiento', '0000-00-00', 50, 162, 50, 15, 20, 'Alprazolam', 22333111, '0000-00-00'),
+('', '', '2017-02-12', '', '', '', '', '', '0000-00-00', 0, 0, 0, 0, 0, '', NULL, '0000-00-00'),
+('Jose', 'Marcano', '2017-11-02', 'Calle tal', 'asjka', 'ajksajsk', '', 'skajskasj', '0000-00-00', 60, 120, 22, 0, 0, 'jkdjsk', 11000000, '0000-00-00'),
+('asals', 'akslaksl', '2018-11-05', 'sasa', 'motivo', 'enfermedad', 'antefam', 'anteper', 'inm', 50, 120, 0, 0, 0, 'sldksld', 12000000, '2018-07-12');
 
 -- --------------------------------------------------------
 
